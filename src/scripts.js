47e325616160a5820830d6472f6a6193d18b455a
Ron Tamir <rontamirh@gmail.com>
2015-05-25 15:17:11 -0400

    Add src/index.html

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,17 @@
-(function () {
-	console.log('Hello, world!');
-})();
+	
+function showDiv() {	
+	document.getElementById('area').style.display = "block"; 
+	document.getElementById("reply").style.visibility = "hidden";
+}
+
+
+$(document).ready(function() {
+	setTimeout(function() {
+		$('#myModal').modal('hide');
+    }, 5500); // milliseconds
+	$('#myModal').on('hidden.bs.modal', function (e) {
+		$('#myModal_sent').modal('show');
+
+	})
+});
+
